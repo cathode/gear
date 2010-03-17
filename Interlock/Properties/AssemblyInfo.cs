c0bb141b4fd73cdf1459725e2eec44d6a8af6975
William Shelley <cathode@live.com>
2010-03-17 17:15:44 +0000

    * Added "Interlock" project to hold to-be-written reusable "virtual world engine".
    git-svn-id: http://localhost/svn/gear/trunk@28 f7f8973e-caa7-11de-a1cf-4bfa7bdbae30

---
+++ Interlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Interlock")]
+[assembly: AssemblyDescription("A virtual world engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Will Shelley")]
+[assembly: AssemblyProduct("Interlock")]
+[assembly: AssemblyCopyright("Copyright © Will Shelley 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0cad66e3-c1af-4210-8746-d2660e318c07")]
+[assembly: AssemblyVersion("0.1.*")]
