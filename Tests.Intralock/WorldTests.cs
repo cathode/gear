c8aef27f320aaa1a95a3578b1e1434cbf24493bb
William Shelley <cathode@live.com>
2010-03-17 19:42:28 +0000

    Added msbuild xunit runner.
    git-svn-id: http://localhost/svn/gear/trunk@33 f7f8973e-caa7-11de-a1cf-4bfa7bdbae30

--- Tests.Intralock/WorldTests.cs
+++ Tests.Intralock/WorldTests.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace Tests.Intralock
 {
     public class WorldTests
+    {
+        [Fact]
+        public void ThisShouldFail()
+        {
+            Assert.Equal<bool>(false, true);
+        }
+
+        [Fact]
+        public void ThisShouldPass()
+        {
+            Assert.Equal<int>(1, 1);
+        }
+    }
+
+    public class World
     {
     }
 }
