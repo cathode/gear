aa1661bd57d85de0199ebccbc50f6ce61aa53e0f
William Shelley <cathode@live.com>
2010-03-18 04:36:10 +0000

    Hopefully fixed xunit build problems.
    git-svn-id: http://localhost/svn/gear/trunk@35 f7f8973e-caa7-11de-a1cf-4bfa7bdbae30

---
+++ Tests.Intralock/EntityTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.Intralock
+{
+    public class EntityTests
+    {
+        /* Entity - Represents an object within a virtual World.
+         * 
+         * Facts about Entities:
+         * 
+         * They exist inside a World.
+         * Some of them can be interacted with.
+         * Some of them have a virtual physical presence.
+         * Entities have runtime-extensible special fields.
+         * The runtime entity fields of an entity are defined by it's schema.
+         * Entities are instances of a schema.
+         */
+    }
+
+    public class Entity
+    {
+
+    }
+}
--- Tests.Intralock/WorldTests.cs
+++ Tests.Intralock/WorldTests.cs
@@ -8,6 +8,17 @@ namespace Tests.Intralock
 {
     public class WorldTests
     {
+        /* World - Represents a virtual environment and everything in it. Basically, a huge global container that directly or indirectly contains everything.
+         * 
+         * Basics about a World:
+         * 
+         * A World contains entities.
+         * A World can't contain another World, that would be MADNESS!
+         * One or more servers host a single World, but a server can only host one World at a time.
+         * The state of a World should be persistable (to a file or database or another server).
+         * The state of a World should support synchronization between servers.
+         * A World instance needs to be thread-safe.
+         */
         [Fact]
         public void ThisShouldFail()
         {
@@ -23,5 +34,6 @@ namespace Tests.Intralock
 
     public class World
     {
+
     }
 }
