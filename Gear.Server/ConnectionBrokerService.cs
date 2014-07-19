760faef6e754d36f64a18a384bd8f0d354974f21
cathode <cathode@live.com>
2014-07-18 18:30:00 -0700

    Added stub classes for server component services.

---
+++ Gear.Server/ConnectionBrokerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gear.Server
+{
+    public class ConnectionBrokerService
+    {
+
+    }
+}
---
+++ Gear.Server/ServiceAnnouncer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace Gear.Server
+{
+    public class ServiceAnnouncer
+    {
+        private List<ServiceAnnouncement> announcements;
+        public ServiceAnnouncer()
+        {
+            this.announcements = new List<ServiceAnnouncement>();
+        }
+
+        /// <summary>
+        /// Adds a service announcement to the current announcer.
+        /// </summary>
+        /// <param name="announcement"></param>
+        public void AddServiceAnnouncement(ServiceAnnouncement announcement)
+        {
+            if (!this.announcements.Any(e => e.LocalPort == announcement.LocalPort))
+            {
+
+            }
+
+            this.announcements.Add(announcement);
+
+        }
+    }
+
+    public class ServiceAnnouncement
+    {
+        public ServerService Service { get; set; }
+
+        public ushort LocalPort { get; set; }
+    }
+}
